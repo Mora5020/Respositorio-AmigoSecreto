765bb2c7ef48fd0afa73a61968da2f455af1ca4d
Mora5020 <moralucaccini@gmail.com>
2025-08-26 18:10:17 -0300

    verificar amigo

--- appOriginal.js
+++ appOriginal.js
@@ -22,8 +22,21 @@ function agregarAmigo(){
 
 document.getElementById('botonAgregar').addEventListener('click', agregarAmigo);
 
+
+
 function obtenerNombre() {
     let input = document.getElementById('amigo').value.trim();
     console.log(input);
     return input;
+}
+
+function verificarAmigo(input) {
+    if (!input || input === '') {
+        alert('Por favor ingresa un nombre');
+        return false;
+    } else if (listaAmigxs.includes(input)) {
+        alert('Este nombre fue ingresado, por favor ingresa otro');
+        return false;
+    }
+    return true;
 }
